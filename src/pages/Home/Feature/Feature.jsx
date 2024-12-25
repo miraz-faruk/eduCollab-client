b77ba7a8313df057cda8d6b9a9c65937326a5fb8
mirazfaruk <mirazfaruk@gmail.com>
2024-12-25 11:58:18 +0600

    first commit

---
+++ src/pages/Home/Feature/Feature.jsx
@@ -0,0 +1,11 @@
+
+
+const Feature = () => {
+    return (
+        <div>
+            <h2>Feature</h2>
+        </div>
+    );
+};
+
+export default Feature;
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,17 @@
+import Banner from "../Banner/Banner";
+import Faq from "../Faq/Faq";
+import Feature from "../Feature/Feature";
+
+
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <Feature></Feature>
+            <Faq></Faq>
+        </div>
+    );
+};
+
+export default Home;
